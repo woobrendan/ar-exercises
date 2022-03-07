59f7da6b5e2d33c6f7171c27f997f4b8b8634dfe
Brendan Woo <woo.brendan@gmail.com>
2022-03-07 21:57:17 +0000

    complete ex 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,22 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+burnaby = Store.new
+richmond = Store.new
+gastown = Store.new
+burnaby.annual_revenue = 300000
+burnaby.mens_apparel = true
+burnaby.womens_apparel = true
+burnaby.save
+richmond.annual_revenue = 1260000
+richmond.mens_apparel = false
+richmond.womens_apparel = true
+gastown.annual_revenue = 190000
+gastown.mens_apparel = true
+gastown.womens_apparel = false
+richmond.save
+gastown.save
+# richmond = Store.new(1260000, false, true,)
+# gastwon = Store.new(190000, true, false)
+
+puts Store.count
