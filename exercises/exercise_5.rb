8723cd36bfd58811e73fe3f19950abb982a5d09d
Brendan Woo <woo.brendan@gmail.com>
2022-03-07 22:57:06 +0000

    complete ex 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,10 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+#Sum of all annual revenue from all stores
+Store.sum("annual_revenue")
+
+#average of all rows in annual revenue column
+puts Store.average(:annual_revenue)
+
+puts Store.where("stores.annual_revenue >= ?", 1000000).count
