1983cb04ef294a5ed17f2a60c67e92ab8dc5f04b
Brendan Woo <woo.brendan@gmail.com>
2022-03-08 01:53:44 +0000

    complete ex 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,12 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+
+#2 ask user for store name
+puts "Enter Store name"
+userStore = $stdin.gets.chomp
+
+store6 = Store.create(name: userStore)
+
+puts store6.errors.full_messages
