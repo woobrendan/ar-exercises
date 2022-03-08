4efd07320fdc8f4a48be08b88aca4a62215b5c61
Brendan Woo <woo.brendan@gmail.com>
2022-03-08 01:54:07 +0000

    add ex 7 validators to each class

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,8 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, length: { in: 40..200}
+  validates_associated :store
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,5 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :annual_revenue, numericality: {greater_than: 0}
+  validates :name, length: { minimum: 3 }
 end
